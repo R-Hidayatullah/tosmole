fdac8415a42db4624fe82c039613651d7856c40e
unknown <24.ridwanhidayatullah.9b@gmail.com>
2024-09-21 09:30:48 +0700

    tidy up broken files

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 #![feature(seek_stream_len)]
 
-use crate::window_render::render;
 
 mod ies_parser;
 mod ipf_parser;
-mod window_render;
 mod xac_parser;
 mod xsm_parser;
 
 fn main() {
     println!("Hello world");
-    render().unwrap();
 }
